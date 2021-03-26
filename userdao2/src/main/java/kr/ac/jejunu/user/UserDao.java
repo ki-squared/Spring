a4eaf78c268229a77ea3eb321fba8e76128c4bc1
abtjade <abt.jade@gmail.com>
2021-03-26 16:48:27 +0900

    Extract get connection Method

--- userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -4,17 +4,7 @@ import java.sql.*;
 
 public class UserDao {
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        // Where is the data stored? MySQL
-
-        // Load Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        // Make Connection
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/kakao?" +
-                        "characterEncoding=utf-8&serverTimezone=UTC"
-                ,"root","root"
-        );
+        Connection connection = getConnection();
 
         // Make Query - Use PreparedStatement to cache only one sentence(the line right below)
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id=?");
@@ -39,12 +29,22 @@ public class UserDao {
         return user;
     }
 
-    public void insert(User user) throws ClassNotFoundException, SQLException {
-        // Where is the Data stored? => MySQL
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        // Where is the data stored? MySQL
+
+        // Load Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/kakao?" +
+
+        // Make Connection
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/kakao?" +
                         "characterEncoding=utf-8&serverTimezone=UTC"
-                ,"root","root");
+                , "root", "root"
+        );
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
         preparedStatement.setString(2, user.getPassword());
