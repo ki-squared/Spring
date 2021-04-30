aa47b19e8f53daa27b5257a117772d4f0c01efdb
abtjade <abt.jade@gmail.com>
2021-04-30 18:29:41 +0900

    Upload class-9 activities(2)

---
+++ review/userdao/src/main/java/kr/ac/jejunu/userdao/UserServlet.java
@@ -0,0 +1,30 @@
+package kr.ac.jejunu.userdao;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UserServlet extends GenericServlet {
+    @Override
+    public void destroy() {
+        System.out.println("*************** destroy *****************");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("*************** init *****************");
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("*************** service *****************");
+        StringBuffer response = new StringBuffer();
+        response.append("<html>");
+        response.append("<body>");
+        response.append("<h1>");
+        response.append("Hello!!!!");
+        response.append("</h1>");
+        response.append("</body>");
+        response.append("</html>");
+        res.getWriter().println(response.toString());
+    }
+}
