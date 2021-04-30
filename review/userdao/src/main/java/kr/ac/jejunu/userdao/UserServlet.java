2609eb91b8979aed3fd645d1be634ed91e4a7fb8
abtjade <abt.jade@gmail.com>
2021-04-30 18:44:10 +0900

    Upload Class 9

--- review/userdao/src/main/java/kr/ac/jejunu/userdao/UserServlet.java
+++ review/userdao/src/main/java/kr/ac/jejunu/userdao/UserServlet.java
@@ -1,9 +1,13 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import javax.servlet.*;
 import java.io.IOException;
 
 public class UserServlet extends GenericServlet {
+    private UserDao userDao;
     @Override
     public void destroy() {
         System.out.println("*************** destroy *****************");
@@ -11,17 +15,23 @@ public class UserServlet extends GenericServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext("kr.ac.jejunu.userdao");
+        userDao = applicationContext.getBean("userDao", UserDao.class);
         System.out.println("*************** init *****************");
     }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("*************** service *****************");
+        Integer id = Integer.parseInt(req.getParameter("id"));
+        User user = userDao.get(id);
+        res.setContentType("text/html; charset=UTF-8");
         StringBuffer response = new StringBuffer();
         response.append("<html>");
         response.append("<body>");
         response.append("<h1>");
-        response.append("Hello!!!!");
+        response.append(String.format("Hello %s!!!", user.getName()));
         response.append("</h1>");
         response.append("</body>");
         response.append("</html>");
