5accede206fdfd4b77fdfd5f6293df9614bd7fa3
abtjade <abt.jade@gmail.com>
2021-05-26 18:48:49 +0900

    Review Lombok

--- userdao_/userdao/src/main/java/kr/ac/jejunu/userdao/User.java
+++ userdao_/userdao/src/main/java/kr/ac/jejunu/userdao/User.java
@@ -1,31 +1,22 @@
 package kr.ac.jejunu.userdao;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Integer id;
     private String name;
     private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
