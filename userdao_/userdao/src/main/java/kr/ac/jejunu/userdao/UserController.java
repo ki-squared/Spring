da1dcf86306a08a4fada56ff0e1560e20bde4bf7
abtjade <abt.jade@gmail.com>
2021-05-28 17:39:34 +0900

    File 저장 static dir, File 제출 프로그래밍 실습

--- userdao_/userdao/src/main/java/kr/ac/jejunu/userdao/UserController.java
+++ userdao_/userdao/src/main/java/kr/ac/jejunu/userdao/UserController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping
@@ -22,6 +26,18 @@ public class UserController {
     @RequestMapping(path="/upload", method = RequestMethod.GET)
     public void upload() {
 
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        String path = request.getServletContext().getRealPath("/")
+                + "/WEB-INF/static/" + file.getOriginalFilename(); // file 이름을 같이 담아!
+        File saveFile = new File(path);
+        FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        return new ModelAndView();
     }
 
     @ExceptionHandler(Exception.class)
